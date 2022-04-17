28a4b1f647086c44138940c0c56766496c614a88
Santilopezc <91813052+Santilopezc@users.noreply.github.com>
2022-04-16 23:41:47 -0500

    Add files via upload
    Segunda Entrega del proyecto. Se implemento el algoritmo de Dijkstra con la libreria netwrokx

--- codigo/proyecto.py
+++ codigo/proyecto.py
@@ -3,8 +3,18 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from shapely import wkt
+import networkx as nx
+
+origin = input('Enter the origin coordinate of the path')
+end = input('Enter the end coordinate of the path')
 
 data = pd.read_csv('calles_de_medellin_con_acoso.csv',sep=';')
+data['geometry'] = data['geometry'].apply(wkt.loads)
+data = gpd.GeoDataFrame(data)
+
+area = pd.read_csv('poligono_de_medellin.csv',sep=';')
+area['geometry'] = area['geometry'].apply(wkt.loads)
+area = gpd.GeoDataFrame(area)
 
 data['harassmentRisk'].fillna(data['harassmentRisk'].mean(), inplace = True)
 
@@ -23,7 +33,46 @@ dataframes = [data1, doble_sentido]
 
 calles = pd.concat(dataframes)
 
-hash_table = defaultdict(list)
-for ind in calles.index:
-    hash_table[data['origin'][ind]].append((data['destination'][ind], data['length'][ind], data['harassmentRisk'][ind]))
 
+
+corto = nx.from_pandas_edgelist(calles, source = 'origin', target = 'destination', edge_attr = 'length')
+recorrido_dist = nx.dijkstra_path(corto, source= origin, target = end, weight = True)
+origenes_dist = recorrido_dist[:-1]
+destinos_dist = recorrido_dist[1:]
+
+acoso = nx.from_pandas_edgelist(calles, source = 'origin', target = 'destination', edge_attr = 'harassmentRisk')
+recorrido_risk = nx.dijkstra_path(acoso, source= origin, target = end, weight = True)
+origenes_risk = recorrido_risk[:-1]
+destinos_risk = recorrido_risk[1:]
+
+#Create plot
+fig, ax = plt.subplots(figsize=(12,8))
+
+# Plot the footprint
+area.plot(ax=ax, facecolor='black')
+
+# Plot street data
+data.plot(ax=ax, linewidth=1, edgecolor='dimgray')
+for i in range(len(origenes_dist)):
+    dist = calles[(calles['origin'] == origenes_dist[i]) & (calles['destination'] == destinos_dist[i])]
+    dist.plot(ax = ax,linewidth=1, edgecolor='y')
+#recorrido.plot(ax=ax, linewidth=1, edgecolor='y')
+plt.title(' Shortest Path')
+plt.tight_layout()
+plt.show()
+
+#Create plot
+fig, ax = plt.subplots(figsize=(12,8))
+
+# Plot the footprint
+area.plot(ax=ax, facecolor='black')
+
+# Plot street data
+data.plot(ax=ax, linewidth=1, edgecolor='dimgray')
+for i in range(len(origenes_risk)):
+    risk = calles[(calles['origin'] == origenes_risk[i]) & (calles['destination'] == destinos_risk[i])]
+    risk.plot(ax = ax,linewidth=1, edgecolor='y')
+#recorrido.plot(ax=ax, linewidth=1, edgecolor='y')
+plt.title('Least Harassment Risk Path')
+plt.tight_layout()
+plt.show()
