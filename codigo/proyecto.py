5a140649dbc4ce1729003143039e5ef7d08ef35e
Santilopezc <91813052+Santilopezc@users.noreply.github.com>
2022-05-18 21:23:16 -0500

    Add files via upload
    Entrega Final

--- codigo/proyecto.py
+++ codigo/proyecto.py
@@ -4,7 +4,9 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from shapely import wkt
 import networkx as nx
+import time
 
+start_time = time.time()
 origin = input('Enter the origin coordinate of the path')
 end = input('Enter the end coordinate of the path')
 
@@ -32,18 +34,14 @@ data1 = data[['name', 'origin', 'destination', 'length', 'harassmentRisk',
 dataframes = [data1, two_way]
 
 streets = pd.concat(dataframes)
+streets['weightedDist'] = streets['length'] * streets['harassmentRisk']
 
 
 
-shortest = nx.from_pandas_edgelist(streets, source = 'origin', target = 'destination', edge_attr = 'length')
+shortest = nx.from_pandas_edgelist(streets, source = 'origin', target = 'destination', edge_attr = 'weightedDist')
 path_shortest = nx.dijkstra_path(shortest, source= origin, target = end, weight = True)
 origins_shortest = path_shortest[:-1]
 destinations_shortest = path_shortest[1:]
-
-harassment = nx.from_pandas_edgelist(streets, source = 'origin', target = 'destination', edge_attr = 'harassmentRisk')
-path_risk = nx.dijkstra_path(harassment, source= origin, target = end, weight = True)
-origins_risk = path_risk[:-1]
-destinations_risk = path_risk[1:]
 
 #Create plot
 fig, ax = plt.subplots(figsize=(12,8))
@@ -54,27 +52,16 @@ area.plot(ax=ax, facecolor='black')
 # Plot street data
 data.plot(ax=ax, linewidth=1, edgecolor='dimgray')
 length = 0
+risk = 0
 for i in range(len(origins_shortest)):
     dist = streets[(streets['origin'] == origins_shortest[i]) & (streets['destination'] == destinations_shortest[i])]
     dist.plot(ax = ax,linewidth=1, edgecolor='y')
     length += dist['length'].values
+    risk += dist['harassmentRisk'].values
 #recorrido.plot(ax=ax, linewidth=1, edgecolor='y')
-plt.title(f'Shortest Path \n Length: {int(length)}')
+exTime = time.time()-start_time
+plt.title(f'Shortest Path \n Length: {int(length)} \n Risk: {int(risk)/len(origins_shortest)} \n Execution Time: {exTime}')
 plt.tight_layout()
 plt.show()
 
-#Create plot
-fig, ax = plt.subplots(figsize=(12,8))
 
-# Plot the footprint
-area.plot(ax=ax, facecolor='black')
-
-# Plot street data
-data.plot(ax=ax, linewidth=1, edgecolor='dimgray')
-for i in range(len(origins_risk)):
-    risk = streets[(streets['origin'] == origins_risk[i]) & (streets['destination'] == destinations_risk[i])]
-    risk.plot(ax = ax,linewidth=1, edgecolor='y')
-#recorrido.plot(ax=ax, linewidth=1, edgecolor='y')
-plt.title('Least Harassment Risk Path')
-plt.tight_layout()
-plt.show()
